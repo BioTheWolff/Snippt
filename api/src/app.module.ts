fdc1e74e0e57f007d2ff66834a85ac8c62297b8c
BioTheWolff <biothewolff@gmx.fr>
2022-12-03 18:54:23 +0100

    feat(orm): adding ORM

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,8 +1,27 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+require('dotenv').config();
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    // ORM module
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DATABASE,
+      entities: [
+          __dirname + '/**/*.entity{.ts,.js}',
+      ],
+      synchronize: (process.env.MODE === "env"),
+    }),
+    // API modules
+    UsersModule,
+  ],
   controllers: [],
   providers: [],
 })
