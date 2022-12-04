a9479999aa27528d113ce922a25940a1e45ff8c3
BioTheWolff <biothewolff@gmx.fr>
2022-12-04 15:59:29 +0100

    refactor(users.entities): adding serialization exclusion and swagger now hiding some properties

--- api/src/users/entities/user.entity.ts
+++ api/src/users/entities/user.entity.ts
@@ -1,25 +1,46 @@
+import { ApiHideProperty } from "@nestjs/swagger";
+import { Exclude } from "class-transformer";
 import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 
 @Entity()
 export class User {
+    @Exclude()
     @PrimaryGeneratedColumn()
+    @ApiHideProperty()
     id: number;
+
 
     @Column({ type: "varchar", length: 30, unique: true })
     handle: string;
+
     @Column({ type: "varchar", length: 40 })
     display_name: string;
-    
+
+
+    @Exclude()
+    @ApiHideProperty()
     @Column({ type: "varchar", length: 80, unique: true })
     email: string;
+
+    @Exclude()
+    @ApiHideProperty()
     @Column({ type: "varchar", length: 255 })
     password: string;
 
-    @Column({ type: "bool" })
+
+    @Exclude()
+    @ApiHideProperty()
+    @Column({ type: "bool", default: false })
     disabled: boolean;
 
+
+    @Exclude()
+    @ApiHideProperty()
     @CreateDateColumn()
     created_at: Date;
+
+    @Exclude()
+    @ApiHideProperty()
     @UpdateDateColumn()
     updated_at: Date;
 }
