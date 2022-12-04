2752380ce7379fd921ef98e2ca6ecdbf6a6bf572
BioTheWolff <biothewolff@gmx.fr>
2022-12-04 15:58:18 +0100

    refactor(user.dto): refactoring the DTOs for the users module

--- api/src/users/dto/create-user.dto.ts
+++ api/src/users/dto/create-user.dto.ts
@@ -1,1 +1,26 @@
-export class CreateUserDto {}
+import { Transform } from 'class-transformer';
+import { IsString, IsNotEmpty, IsEmail, Length } from 'class-validator';
+
+export class CreateUserDto {
+    @IsString()
+    @IsNotEmpty()
+    @Length(1, 30)
+    @Transform(({ value }) => value.toLowerCase())
+    handle: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @Length(1, 40)
+    display_name: string;
+    
+    @IsEmail()
+    @IsNotEmpty()
+    @Length(6, 80)
+    @Transform(({ value }) => value.toLowerCase())
+    email: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @Length(8)
+    password: string;
+}
--- api/src/users/dto/update-user.dto.ts
+++ api/src/users/dto/update-user.dto.ts
@@ -1,4 +1,10 @@
-import { PartialType } from '@nestjs/swagger';
+import { OmitType, PartialType } from '@nestjs/swagger';
 import { CreateUserDto } from './create-user.dto';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(
+    OmitType(CreateUserDto, ['password'] as const),
+  ) {
+    isEmpty() {
+      return !(this.email || this.display_name || this.handle);
+    }
+  }
