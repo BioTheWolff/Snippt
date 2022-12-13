6b25bf24feba708223916fdf8a13438392f214f3
BioTheWolff <biothewolff@gmx.fr>
2022-12-13 20:45:27 +0100

    feat(posts): adding auth on routes and implementing create controller method

--- api/src/posts/posts.controller.ts
+++ api/src/posts/posts.controller.ts
@@ -1,15 +1,17 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Req } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
+import { Request as RequestType } from 'express';
+import { User } from '../users/entities/user.entity';
 
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
   @Post()
-  create(@Body() createPostDto: CreatePostDto) {
-    return this.postsService.create(createPostDto);
+  create(@Req() req: RequestType & { user: User }, @Body() createPostDto: CreatePostDto) {
+    return this.postsService.create(req.user, createPostDto);
   }
 
   @Get()
--- api/src/posts/posts.module.ts
+++ api/src/posts/posts.module.ts
@@ -3,10 +3,19 @@ import { PostsService } from './posts.service';
 import { PostsController } from './posts.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Post } from './entities/post.entity';
+import { AuthModule } from '../auth/auth.module';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Post])],
+  imports: [TypeOrmModule.forFeature([Post]), AuthModule],
   controllers: [PostsController],
-  providers: [PostsService]
+  providers: [
+    PostsService,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard
+    }
+  ]
 })
 export class PostsModule {}
