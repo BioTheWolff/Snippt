03a8c3f81047192ec49d336bb19c6aff33c3739e
BioTheWolff <biothewolff@gmx.fr>
2022-12-13 20:23:18 +0100

    feat(posts.entities): updating the entity

--- api/src/posts/entities/post.entity.ts
+++ api/src/posts/entities/post.entity.ts
@@ -1,1 +1,36 @@
-export class Post {}
+import { ApiProperty } from "@nestjs/swagger";
+import { Exclude } from "class-transformer";
+import { Column, CreateDateColumn, PrimaryGeneratedColumn } from "typeorm";
+
+export class Post {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+
+    // Description
+    @Column('varchar', { length: 60 })
+    title: string;
+
+    @Column('varchar', { length: 250 })
+    description: string;
+
+
+    // Main content (snippet)
+    @Column('varchar', { length: 20 })
+    language: string;
+
+    @Column('text')
+    @ApiProperty({ maxLength: 2000 })
+    content: string;
+
+
+    // Authoring
+    @CreateDateColumn()
+    created_at: number;
+
+    
+    // State
+    @Exclude()
+    @Column('integer', { default: 0 })
+    deleted: boolean;
+}
