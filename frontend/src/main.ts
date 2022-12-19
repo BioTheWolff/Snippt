3e6dcd0ae1889feccb2c81d27560e2ffbbfd4085
BioTheWolff <biothewolff@gmx.fr>
2022-12-19 21:19:06 +0100

    feat(frontend): adding Oruga and bulma support

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,14 +1,22 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { bulmaConfig } from '@oruga-ui/theme-bulma'
+import Oruga from '@oruga-ui/oruga-next'
 
+// local init files
 import App from './App.vue'
 import router from './router'
 
+// global styles
+import '@oruga-ui/theme-bulma/dist/bulma.css'
 import './assets/main.css'
 
+
+// app init
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(Oruga, bulmaConfig);
 app.use(router)
 
 app.mount('#app')
