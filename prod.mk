2ecea675b0105a3b2bbfe1f7c63b62cbe0c4a5c2
BioTheWolff <biothewolff@gmx.fr>
2023-01-04 18:48:35 +0100

    feat(makefile): adding build to prod

--- prod.mk
+++ prod.mk
@@ -8,6 +8,12 @@ up:
 down:
 	@cd .docker && docker compose -f docker-compose.yml -f docker-compose.prod.yml down
 
+build:
+	@cd .docker && docker compose -f docker-compose.yml -f docker-compose.prod.yml build
+
+redeploy: build
+	@cd .docker && docker compose -f docker-compose.yml -f docker-compose.prod.yml up -d --force-recreate
+
 
 # MIGRATE #
 migrate: up
